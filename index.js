4ff4ed1cf31c30f2643175a3abd9a7248196eb8b
asuforce <owata.sn@gmail.com>
2018-07-19 17:54:16 +0900

    use Vue.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-var map;
-
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {
-      lat: 34.7019399,
-      lng: 135.51002519999997
-    },
-    zoom: 19
-  });
-}
+new Vue({
+  el: '#app',
+  data: {
+    name: 'hoge',
+    place: ''
+  },
+  methods: {
+    sendPlace: function () {
+      alert(this.place);
+    }
+  }
+});
