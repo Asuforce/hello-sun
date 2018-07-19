56427bd56959cb162c112f8a32397644f3ec0990
asuforce <owata.sn@gmail.com>
2018-07-19 17:07:44 +0900

    add GoogleMap

---
+++ index.js
@@ -0,0 +1,11 @@
+var map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {
+      lat: 34.7019399,
+      lng: 135.51002519999997
+    },
+    zoom: 19
+  });
+}
