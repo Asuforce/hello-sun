ceb0ae3716a56c051c2b3379873c24eac888d629
asuforce <owata.sn@gmail.com>
2018-07-25 17:33:43 +0900

    goodbye vue.js

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-var name = "hoge";
-
-new Vue({
-  el: '#app',
-  data: {
-    name: name,
-    place: ''
-  },
-  methods: {
-    sendPlace: function () {
-      alert(this.place);
-    }
-  }
-});
-
 window.onload = function (e) {
   liff.init(function (data) {
     initializeApp(data);
@@ -21,16 +6,16 @@ window.onload = function (e) {
 
 function initializeApp(data) {
   liff.getProfile().then(function (profile) {
-    name = profile.displayName;
+    document.getElementById('name').textContent = profile.displayName;
 
-    var profilePirctureDiv = document.getElementById('profilepicturediv');
-    if (profilePirctureDiv.firstElementChild) {
-      profilePirctureDiv.removeChild(profilePirctureDiv.firstElementChild);
+    var picture = document.getElementById('picture');
+    if (picture.firstElementChild) {
+      picture.removeChild(picture.firstElementChild);
     }
     var img = document.createElement('img');
     img.src = profile.pictureUrl;
     img.alt = "Profile Picture";
-    profilePirctureDiv.appendChild(img);
+    picture.appendChild(img);
   }).catch(function (error) {
     window.alert("Error getting profile: " + error);
   })
