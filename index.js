727d35b02cca74b9f67de76ed0ae9ebefee79d3e
asuforce <owata.sn@gmail.com>
2018-08-06 14:30:57 +0900

    Search geometrics

--- index.js
+++ index.js
@@ -21,3 +21,56 @@ function initializeApp(data) {
     window.alert("Error getting profile: " + error);
   })
 }
+
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: { lat: 35.709, lng: 139.731 },
+    zoom: 7
+  });
+}
+
+var getMap = (function () {
+  function codeAddress(address) {
+    var geocoder = new google.maps.Geocoder();
+
+    var mapOptions = {
+      zoom: 18,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      scrollwheel: false
+    };
+
+    map.setZoom(18);
+
+    geocoder.geocode({ 'address': address, 'region': 'jp' }, function (results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        map.setCenter(results[0].geometry.location);
+
+        var marker = new google.maps.Marker({
+          map: map,
+          position: results[0].geometry.location
+        });
+      } else {
+        window.alert("入力された場所が見つかりませんでした。");
+      }
+    });
+  }
+
+  return {
+    getAddress: function () {
+      var button = document.getElementById("button");
+
+      button.onclick = function () {
+        var address = document.getElementById("address").value;
+
+        if (address) {
+          codeAddress(address);
+        } else {
+          window.alert("場所を入力してください。");
+        }
+      }
+    }
+  };
+})();
+
+getMap.getAddress();
